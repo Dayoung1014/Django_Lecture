e87f4e8fa7bc6a22f3606a773c8ac8d7d32a30a6
Dayoung1014 <wsqqa1014@qmail.com>
2021-09-28 23:46:18 +0900

    CBV로 블로그 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),  # path(''. ) 현재 위치가 서버IP/blog
-    path('<int:pk>/', views.singel_post_page),  # 서버IP/blog/pk
+    #path('', views.index),  # path(''. ) 현재 위치가 서버IP/blog
+    path('', views.PostList.as_view()), #PostList (모델이름 + List)
+    #path('<int:pk>/', views.singel_post_page),  # 서버IP/blog/pk
+    path('<int:pk>/', views.PostDetail.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,35 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
-def index(request):
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    #template_name = 'blog/post_list.html'   #cbv는 직접적으로 적어줄 필요는 없지만 template_name인 경우 내가 정해둔 템플릿으로 적용 가능
+    # post_list.html 지동으로 연결
+
+class PostDetail(DetailView):
+    model = Post
+    #template_name = 'blog/post_detail.html'
+    # post_detail.html 자동으로 연결
+
+'''def index(request):
     posts = Post.objects.all().order_by('-pk') # model에 저장된 post 모두 (all) 가져오기
     # .order_by('-pk') -pk 최신순 정렬
     # .order_by('pk') -pk 작성순 정렬
 
-    return render(request,'blog/index.html',
+    return render(request,'blog/post_list.html',
                   {
                       'posts' : posts,
                   }
-                  )
+                  )'''
 
-def singel_post_page(request, pk):
+'''def singel_post_page(request, pk):
     post = Post.objects.get(pk = pk) # 해당되는 pk post 가져오기
 
-    return render(request,'blog/singel_post_page.html',
+    return render(request,'blog/post_detail.html',
                   {
                       'post' : post,
                   }
-                  )
+                  )'''
